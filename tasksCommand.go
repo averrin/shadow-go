e382fefea1f5e946ee0ad86378742b311bb16099
Alexey Nabrodov <alexey.nabrodov@dev.zodiac.tv>
2016-06-03 13:34:47 +0300

    switch list

--- tasksCommand.go
+++ tasksCommand.go
@@ -25,11 +25,17 @@ func (Cmd *TasksCommand) Init() {
 }
 
 func (Cmd *TasksCommand) Test(line string) bool {
+	if strings.HasPrefix(line, "switch ") {
+		line = strings.Split(line, " ")[1]
+	}
 	_, ok := Cmd.Mapping[strings.ToLower(line)]
 	return ok
 }
 
 func (Cmd *TasksCommand) Exec(line string) int {
+	if strings.HasPrefix(line, "switch ") {
+		line = strings.Split(line, " ")[1]
+	}
 	return Cmd.Mapping[strings.ToLower(line)](line)
 }
 
@@ -39,6 +45,9 @@ func (Cmd *TasksCommand) GetText(line string) Line {
 
 func (Cmd *TasksCommand) GetSuggests(line string) []AutocompleteItem {
 	s := []AutocompleteItem{}
+	if strings.HasPrefix(line, "switch ") {
+		line = strings.Split(line, " ")[1]
+	}
 	for c := range Cmd.Mapping {
 		if strings.HasPrefix(c, line) {
 			s = append(s, AutocompleteItem{Cmd, c})
